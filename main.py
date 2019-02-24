b9204be83a494b606f0f01eb95b8c4933a669d2c
iotarepeat <dhruvam15@gmail.com>
2019-02-24 14:31:51 +0530

    Updated code comments

--- main.py
+++ main.py
@@ -14,35 +14,34 @@ from pyftpdlib.servers import ThreadedFTPServer
 
 
 """
-    TODO:
-    1) Create threads for sha1 as big files will take time
-    2) Use threaded FTPServer 
-    3) Write logic to create unique folder mappings eg
-        dir2 = canjsk
-        dir2/dir2 = ckamskm
-        dir2/dir1 = safnj
-        Which will be used as userID 
+    TODO: Add logic for deletion of files from server -> client
 """
 
 
 class Server(ThreadedFTPServer):
     def __init__(self, ip="127.0.0.1", port=9090, directory=os.getcwd()):
-        genAndDump = lambda x: self.dumpDB(
-            self.generateDB()
-        )  # Utility function to refresh db on closed connection
+        """
+            Initmethod initializes ftp server
+        """
+        # Utility function to refresh db
+        genAndDump = lambda x: self.dumpDB(self.generateDB())
+
+        # Logic for authentication
         authorizer = DummyAuthorizer()
+
         # TODO: Replace with unique username,password for unique homedirs
         authorizer.add_user("user", "12345", homedir=directory, perm="elradfmw")
-        self.cwd = Path(directory)
 
+        self.cwd = Path(directory)
         handler = FTPHandler
         handler.authorizer = authorizer
         handler.on_disconnect = genAndDump  # Refresh DB on close
-        handler.on_connect = genAndDump
+        handler.on_connect = genAndDump  # Refresh DB on connect
 
+        # Configure ip, port & Authentication for server
         super().__init__((ip, port), handler)
 
-    def generateDB(self,dir="."):
+    def generateDB(self, dir="."):
         """
             Creates a dictionary of directory tree(all files) in dir(default=current dir):
             key: Complete file path.
@@ -50,31 +49,39 @@ class Server(ThreadedFTPServer):
             value: sha1sum of the respective file
         """
         try:
-            os.mkdir(self.cwd/'.sync')
+            os.mkdir(self.cwd / ".sync")
             logging.info("Created .sync dir")
         except FileExistsError:
             pass
+
+        # Create a list of all files in cwd
         flat_files = [
             path + "/" + File
             for path, _, fileNames in os.walk(dir)
             for File in fileNames
             if path != "./.sync"
         ]
+
+        # A helper function that stores SHA1 hash of given file in db
         def calcHash(File):
             with open(File, "rb") as f:
                 db[File] = sha1(f.read()).hexdigest()
+
         db = {}
+
+        # Start 5 threads, to perform hashing
         pool = ThreadPool(processes=5)
-        pool.map(calcHash,flat_files)
+        pool.map(calcHash, flat_files)
         pool.close()
+
         logging.debug("db = " + str(db))
         return db
 
-    def dumpDB(self,db):
+    def dumpDB(self, db):
         """
             Writes db  dictionary to ./.sync/sync
         """
-        with open(self.cwd/'.sync/sync', "wb") as f:
+        with open(self.cwd / ".sync/sync", "wb") as f:
             pickle.dump(db, f)
         logging.info("Successfully written db")
 
@@ -94,12 +101,19 @@ class Client(Server):
         user="user",
         password="12345",
         logging_level=logging.INFO,
-        directory=os.getcwd()
+        directory=os.getcwd(),
     ):
-        self.cwd=Path(directory)
+        """
+            Initiate ftp connections to ip:port
+            with user:password
+            directory is local directory that represents remote directory
+        """
         logging.basicConfig(level=logging_level)
+        self.cwd = Path(directory)
+
         self.ftp = FTP("")
         self.ftp.connect(ip, port)
+
         logging.info("Logging in with {}:{}".format(user, password))
         self.ftp.login(user=user, passwd=password)
 
@@ -108,35 +122,38 @@ class Client(Server):
             Open sync and store in db (previous)
                 If doesn't exist, generate and return
             Overwrite sync with remote sync file 
-                Read remote_sync to remote
+                Read remote_sync file to remote dict
         """
         # Check for previous db
-        if os.path.exists(self.cwd/"./.sync/sync"):
-            with open(self.cwd/"./.sync/sync", "rb") as f:
+        if os.path.exists(self.cwd / "./.sync/sync"):
+            with open(self.cwd / "./.sync/sync", "rb") as f:
                 db = pickle.load(f)
         else:
             # Else generate
             db = self.db = self.generateDB()
+
         # Get remote db, overwrite local sync file
         self.downloadFile("./.sync/sync")
-        with open(self.cwd/"./.sync/sync", "rb") as f:
+        with open(self.cwd / "./.sync/sync", "rb") as f:
             remote = pickle.load(f)
         logging.debug("db = {}\nremote = {}".format(db, remote))
         return db, remote
 
     def deleteFile(self, filename):
+        # TODO: Add logic to delete empty directories
         logging.info("Deleting from  remote " + filename)
         self.ftp.delete(filename[2:])  # 2: Is to remove './'
 
     def uploadFile(self, filename):
-        self.ftp.storbinary("STOR " + filename, open(self.cwd/filename, "rb"))
+        self.ftp.storbinary("STOR " + filename, open(self.cwd / filename, "rb"))
         logging.info("Uploaded " + filename)
 
     def downloadFile(self, filename):
         # Create directory if doesn't exist
-        if not os.path.exists(os.path.dirname(self.cwd/filename)):
-            os.makedirs(os.path.dirname(self.cwd/filename))
-        localfile = open(self.cwd/filename, "wb")
+        if not os.path.exists(os.path.dirname(self.cwd / filename)):
+            os.makedirs(os.path.dirname(self.cwd / filename))
+
+        localfile = open(self.cwd / filename, "wb")
         self.ftp.retrbinary("RETR " + filename, localfile.write, 1024)
         localfile.close()
         logging.info("Downloaded " + filename)
@@ -145,6 +162,7 @@ class Client(Server):
         """
             Get unix timestamp (since epoch) from remote system
             of given file_name
+            Otherwise None
         """
         file_name = file_name[2:]
         x = self.ftp.mlsd("", ["modify"])
@@ -152,20 +170,28 @@ class Client(Server):
         for f, mtime in x:
             if f == file_name:
                 ts = mtime["modify"]
-                time = (
+                return (
                     datetime.strptime(ts, "%Y%m%d%H%M%S")
                     .replace(tzinfo=timezone.utc)
                     .timestamp()
                 )
-                return time
 
     def sync(self):
-        prev_db, remote = self.get_db()
-        db = self.db = self.generateDB()
+        """
+            This function will sync remote and local dir
+            TODO: Deletion of remote files on local
+        """
+        prev_db, remote = self.get_db()  # Initialize remote_db and prev_db
+        db = self.db = self.generateDB()  # Get a fresh copy of current DB
         perfect_files = []
         for i in db:
             if prev_db.get(i, False):
                 # Check if file has been deleted
+                # A file is considered deleted if it exists in prev db but does
+                # not exist in current db
+
+                # Note this removes files which are 100% known not deleted
+                # Actual deletion happens later
                 del prev_db[i]
             if db[i] == remote.get(i):
                 # Exactly same sha1hash
@@ -177,7 +203,7 @@ class Client(Server):
             else:
                 # SHA1 mismatch. Sync according to newer file
                 perfect_files.append(i)
-                if self.getTimestamp(i) > os.stat(self.cwd/filename).st_mtime:
+                if self.getTimestamp(i) > os.stat(self.cwd / i).st_mtime:
                     logging.info("Downloading {} since new".format(i))
                     self.downloadFile(i)
                 else:
@@ -193,7 +219,7 @@ class Client(Server):
         for i in remote:
             # Download missing files on local
             self.downloadFile(i)
-        # Refresh DB
+        # Write current DB to file
         self.dumpDB(self.db)
 
 
